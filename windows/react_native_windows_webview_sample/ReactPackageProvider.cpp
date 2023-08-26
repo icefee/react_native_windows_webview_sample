909655a1a94e63b2f5860e4b25cd82732cdf5d10
ying_123 <zhangying@aten-optics.com>
2023-08-26 18:23:23 +0800

    add

---
+++ windows/react_native_windows_webview_sample/ReactPackageProvider.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "ReactPackageProvider.h"
+#include "NativeModules.h"
+
+using namespace winrt::Microsoft::ReactNative;
+
+namespace winrt::react_native_windows_webview_sample::implementation
+{
+
+void ReactPackageProvider::CreatePackage(IReactPackageBuilder const &packageBuilder) noexcept
+{
+    AddAttributedModules(packageBuilder, true);
+}
+
+} // namespace winrt::react_native_windows_webview_sample::implementation
